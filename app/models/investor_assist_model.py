5d06fe075aca6275d77c1e0b333b723501eced50
Amit Kumar <amtcuo8579@gmail.com>
2026-03-10 17:15:56 +0530

    issues resolved

--- app/models/investor_assist_model.py
+++ app/models/investor_assist_model.py
@@ -43,8 +43,7 @@ class InvestorAssistant(Base):
         back_populates="investor_assistant",
         uselist=False,
         primaryjoin="InvestorAssistant.investor_assistant_id == foreign(Address.user_id)",
-        overlaps="address",
-        overlaps= "address,admin",
+        overlaps="address,admin",
         # viewonly=True,
     )
 
