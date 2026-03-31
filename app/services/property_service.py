b1422b7787d677b458e16b161ecc0ea88bb878e9
Amit Kumar <amtcuo8579@gmail.com>
2026-03-31 13:46:02 +0530

    added total unit in unit add

--- app/services/property_service.py
+++ app/services/property_service.py
@@ -942,7 +942,7 @@ class PropertyUnitServices:
 
     async def add_new_property_unit(db, data, user_id):
         get_available = await PropertyUnit.get_all_available_by_ids(db, data.property_id, data.unit_type_id)
-        if get_available == data.total_required:
+        if get_available >= data.total_required:
             raise HTTPException(400, "you have added remt for all the unit for this type.")
         new_unit = PropertyUnit(
             unit_id = generate_id("unit"),
